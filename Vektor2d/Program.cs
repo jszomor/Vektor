6e86a67160280588dc82f86d3de06e64b8045101
János Szomor <jszomor@organicawater.com>
2020-02-18 22:39:22 +0100

    init

---
+++ Vektor2d/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vektor2d
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Vektor a = new Vektor(1, 2);
+			Vektor b = new Vektor(3, -1);
+			Vektor c = a+b;
+			Console.WriteLine($"{c.X} {c.Y}");
+
+			Vektor d = a.Osszead2(b);
+			Console.WriteLine($"{d.X} {d.Y}");
+
+			a.Osszead3(b);
+			Console.WriteLine($"{a.X} {a.Y}");
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Vektor2d/Vektor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vektor2d
+{
+	public class Vektor
+	{
+		public double X;
+		public double Y;
+		public Vektor(double x, double y)
+		{
+			X = x;
+			Y = y;
+		}
+		public static Vektor operator+(Vektor a, Vektor b)
+		{
+			return osszead1(a, b);
+		}
+		public static Vektor osszead1 (Vektor a, Vektor b)
+		{
+			Vektor v = new Vektor(a.X + b.X, a.Y + b.Y);
+
+			return v;
+		}
+		public Vektor Osszead2 (Vektor masik)
+		{
+			Vektor v = new Vektor(X + masik.X, Y + masik.Y);
+
+			return v;
+		}
+
+		public void Osszead3 (Vektor masik)
+		{
+			X = X + masik.X;
+			Y = Y + masik.Y;
+		}
+	}
+}
